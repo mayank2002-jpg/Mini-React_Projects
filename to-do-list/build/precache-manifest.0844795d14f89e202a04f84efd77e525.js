cdd61ad94e29e8d8a97a0195dc57727dfb8e248d
mayank2002-jpg <mayankmahajan936@gmail.com>
2022-07-19 21:30:05 +0530

    To-Do List

---
+++ to-do-list/build/precache-manifest.0844795d14f89e202a04f84efd77e525.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83eb9c077a404a978ead3496f3543f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d6363e9543955319f4b",
+    "url": "/static/js/2.aaa56f9c.chunk.js"
+  },
+  {
+    "revision": "4a328453fff5ebc99c3c",
+    "url": "/static/js/main.6fb0023d.chunk.js"
+  },
+  {
+    "revision": "f768c9e2a41fcf84ed92",
+    "url": "/static/js/runtime-main.a20cefaf.js"
+  }
+]);
